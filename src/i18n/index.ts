4080fb9e0394a364c8f3e17dbc7366b47091b086
MohsenDastaran <mohsen.dastaran@gmail.com>
2022-07-30 13:55:51 +0430

    working on i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,52 +3,59 @@ import pinia from '/@/stores/index';
 import { storeToRefs } from 'pinia';
 import { useThemeConfig } from '/@/stores/themeConfig';
 import zhcnLocale from 'element-plus/lib/locale/lang/zh-cn';
+import faLocale from 'element-plus/lib/locale/lang/fa';
 import enLocale from 'element-plus/lib/locale/lang/en';
 import zhtwLocale from 'element-plus/lib/locale/lang/zh-tw';
 
 import nextZhcn from '/@/i18n/lang/zh-cn';
-import nextEn from '/@/i18n/lang/en';
+import next from '/@/i18n/lang/en';
 import nextZhtw from '/@/i18n/lang/zh-tw';
 
-import pagesLoginZhcn from '/@/i18n/pages/login/zh-cn';
-import pagesLoginEn from '/@/i18n/pages/login/en';
-import pagesLoginZhtw from '/@/i18n/pages/login/zh-tw';
+import login from '/@/i18n/pages/login/en';
 import pagesFormI18nZhcn from '/@/i18n/pages/formI18n/zh-cn';
 import pagesFormI18nEn from '/@/i18n/pages/formI18n/en';
 import pagesFormI18nZhtw from '/@/i18n/pages/formI18n/zh-tw';
 
 const messages = {
+	[faLocale.name]: {
+		...faLocale,
+		message: {
+			...next.en,
+			...login.en,
+			...pagesFormI18nZhcn,
+		},
+	},
 	[zhcnLocale.name]: {
 		...zhcnLocale,
 		message: {
-			...nextZhcn,
-			...pagesLoginZhcn,
+			...next['zh-cn'],
+			...login['zh-cn'],
 			...pagesFormI18nZhcn,
 		},
 	},
 	[enLocale.name]: {
 		...enLocale,
 		message: {
-			...nextEn,
-			...pagesLoginEn,
+			...next.en,
+			...login.en,
 			...pagesFormI18nEn,
 		},
 	},
 	[zhtwLocale.name]: {
 		...zhtwLocale,
 		message: {
-			...nextZhtw,
-			...pagesLoginZhtw,
+			...next.zhtw,
+			...login.zhtw,
 			...pagesFormI18nZhtw,
 		},
 	},
 };
+console.log(messages);
 
 // 读取 pinia 默认语言
 const stores = useThemeConfig(pinia);
 const { themeConfig } = storeToRefs(stores);
 
-// 导出语言国际化
 // https://vue-i18n.intlify.dev/guide/essentials/fallback.html#explicit-fallback-with-one-locale
 export const i18n = createI18n({
 	silentTranslationWarn: true,
--- src/i18n/pages/login/en.ts
+++ src/i18n/pages/login/en.ts
@@ -1,29 +1,85 @@
 // 定义内容
 export default {
-	label: {
-		one1: 'User name login',
-		two2: 'Mobile number',
+	en: {
+		label: {
+			one1: 'User name login',
+			two2: 'Mobile number',
+		},
+		link: {
+			one3: 'Third party login',
+			two4: 'Links',
+		},
+		account: {
+			accountPlaceholder1: 'The user name admin or not is common',
+			accountPlaceholder2: 'Password: 123456',
+			accountPlaceholder3: 'Please enter the verification code',
+			accountBtnText: 'Sign in',
+		},
+		mobile: {
+			placeholder1: 'Please input mobile phone number',
+			placeholder2: 'Please enter the verification code',
+			codeText: 'Get code',
+			btnText: 'Sign in',
+			msgText:
+				'Warm tip: it is recommended to use Google, Microsoft edge, version 79.0.1072.62 and above browsers, and 360 browser, please use speed mode',
+		},
+		scan: {
+			text: 'Open the mobile phone to scan and quickly log in / register',
+		},
+		signInText: 'welcome back!',
 	},
-	link: {
-		one3: 'Third party login',
-		two4: 'Links',
+	'zh-cn': {
+		label: {
+			one1: '用户名登录',
+			two2: '手机号登录',
+		},
+		link: {
+			one3: '第三方登录',
+			two4: '友情链接',
+		},
+		account: {
+			accountPlaceholder1: '用户名 admin 或不输均为 common',
+			accountPlaceholder2: '密码：123456',
+			accountPlaceholder3: '请输入验证码',
+			accountBtnText: '登 录',
+		},
+		mobile: {
+			placeholder1: '请输入手机号',
+			placeholder2: '请输入验证码',
+			codeText: '获取验证码',
+			btnText: '登 录',
+			msgText: '* 温馨提示：建议使用谷歌、Microsoft Edge，版本 79.0.1072.62 及以上浏览器，360浏览器请使用极速模式',
+		},
+		scan: {
+			text: '打开手机扫一扫，快速登录/注册',
+		},
+		signInText: '欢迎回来！',
 	},
-	account: {
-		accountPlaceholder1: 'The user name admin or not is common',
-		accountPlaceholder2: 'Password: 123456',
-		accountPlaceholder3: 'Please enter the verification code',
-		accountBtnText: 'Sign in',
+	zhtw: {
+		label: {
+			one1: '用戶名登入',
+			two2: '手機號登入',
+		},
+		link: {
+			one3: '協力廠商登入',
+			two4: '友情連結',
+		},
+		account: {
+			accountPlaceholder1: '用戶名admin或不輸均為common',
+			accountPlaceholder2: '密碼：123456',
+			accountPlaceholder3: '請輸入驗證碼',
+			accountBtnText: '登入',
+		},
+		mobile: {
+			placeholder1: '請輸入手機號',
+			placeholder2: '請輸入驗證碼',
+			codeText: '獲取驗證碼',
+			btnText: '登入',
+			msgText: '* 溫馨提示：建議使用穀歌、Microsoft Edge，版本79.0.1072.62及以上瀏覽器，360瀏覽器請使用極速模式',
+		},
+		scan: {
+			text: '打開手機掃一掃，快速登錄/注册',
+		},
+		signInText: '歡迎回來！',
 	},
-	mobile: {
-		placeholder1: 'Please input mobile phone number',
-		placeholder2: 'Please enter the verification code',
-		codeText: 'Get code',
-		btnText: 'Sign in',
-		msgText:
-			'Warm tip: it is recommended to use Google, Microsoft edge, version 79.0.1072.62 and above browsers, and 360 browser, please use speed mode',
-	},
-	scan: {
-		text: 'Open the mobile phone to scan and quickly log in / register',
-	},
-	signInText: 'welcome back!',
 };
