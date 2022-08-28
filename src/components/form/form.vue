594e3d2a989275d67e0eccbfeacd12cf1272ffdc
MohsenDastaran <mohsen.dastaran@gmail.com>
2022-08-28 15:46:07 +0430

    formObject reactivity bug fixed :)

--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, ref, h, reactive, mergeProps, getCurrentInstance, computed, provide } from 'vue';
+import { defineComponent, ref, h, reactive, mergeProps, computed, provide, watch } from 'vue';
 import { FormItemProp, ElForm } from 'element-plus';
 import twoStateInput from '/@/components/form/twoStateInput.vue';
 import inputWithValidation from '/@/components/form/inputWithValidation.vue';
@@ -11,6 +11,7 @@ export default defineComponent({
 	name: 'form',
 	components: {},
 	props: {
+		test: String,
 		model: {
 			type: Object,
 			required: true,
@@ -21,61 +22,67 @@ export default defineComponent({
 		},
 	},
 	setup(props, { emit, slots }) {
+		// setInterval(() => {
+		// 	console.log(props.label);
+		// }, 1000);
+
 		const twoStateInputValidation = ref(false);
+
 		// using provide because props is not reactive using render function
 		provide(
 			'username',
 			computed(() => twoStateInputValidation.value)
 		);
 		const model = reactive(props.model as any);
-		const inputs = props.formObject.map((item: any) => {
-			const properties = item.props;
+		const inputs = () =>
+			props.formObject.map((item: any) => {
+				const properties = item.props;
 
-			switch (item.type) {
-				case 'inputWithValidation':
-					return h(inputWithValidation, {
-						...properties,
-						onInputChange: (val: string) => {
-							model[properties.prop] = val;
-						},
-					});
-				case 'mixedInput':
-					// merge props using spread operator (...)
-					return h(mixedInput, {
-						...properties,
-						onMixedInputChange: (val: string) => {
-							model[properties.prop] = val;
-						},
-					});
+				switch (item.type) {
+					case 'inputWithValidation':
+						return h(inputWithValidation, {
+							...properties,
+							onInputChange: (val: string) => {
+								model[properties.prop] = val;
+							},
+						});
+					case 'mixedInput':
+						// merge props using spread operator (...)
+						return h(mixedInput, {
+							...properties,
+							onMixedInputChange: (val: string) => {
+								model[properties.prop] = val;
+							},
+						});
 
-				case 'twoStateInput':
-					// merge props using mergeProps
-					const merged = mergeProps(properties, {
-						isValid: twoStateInputValidation.value,
-						onMixedInputChange: (val: string) => {
-							model[properties.prop] = val;
-						},
-						onTwoStateConfirm: (val: string) => {},
-					});
-					return h(twoStateInput, merged);
-				case 'select':
-					return h(select, {
-						...properties,
-						onSelected: (val: string) => {
-							console.log(val);
-							// model[properties.prop] = val;
-						},
-					});
-				case 'checkboxInput':
-					return h(checkboxInput, {
-						...properties,
-						onCheckboxChange: (object: { name: string; values: string[] }) => {
-							model[object.name] = object.values;
-							emit('formChange', model);
-						},
-					});
-			}
-		});
+					case 'twoStateInput':
+						// merge props using mergeProps
+						const merged = mergeProps(properties, {
+							isValid: twoStateInputValidation.value,
+							onMixedInputChange: (val: string) => {
+								model[properties.prop] = val;
+							},
+							onTwoStateConfirm: (val: string) => {},
+						});
+						return h(twoStateInput, merged);
+					case 'select':
+						return h(select, {
+							...properties,
+							onSelected: (object: { name: string; value: string }) => {
+								model[object.name] = object.value;
+								emit('formChange', model);
+							},
+						});
+					case 'checkboxInput':
+						return h(checkboxInput, {
+							...properties,
+							onCheckboxChange: (object: { name: string; values: string[] }) => {
+								model[object.name] = object.values;
+								emit('formChange', model);
+							},
+						});
+				}
+			});
 		const vNode = h(
 			ElForm,
 			{
@@ -83,16 +90,20 @@ export default defineComponent({
 				statusIcon: true,
 				onValidate: (prop: any, isValid: boolean, message: string) => {
 					twoStateInputValidation.value = isValid;
-					// console.log(prop, isValid, message);
 					if (isValid) {
 						emit('formChange', model);
 					}
 				},
 			},
 			// slots.prepend ? h('div', slots.prepend()) : null,
-			inputs
+			inputs()
 			// slots.append ? h('div', slots.append()) : null
 		);
+
+		const object = computed(() => props.formObject);
+		watch(object, () => {
+			vNode.children = inputs();
+		});
 		return () => vNode;
 	},
 });
